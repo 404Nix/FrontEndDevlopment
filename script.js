d5a4b2b0ca4a1249989de94d3fcd28a4257a26d7
Nikhil kanojia <nikhilkanojia693@gmail.com>
2024-08-03 02:40:32 +0530

    project complete

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 var crsr = document.querySelector("#cursor");
 var crsrBlur = document.querySelector("#cursor-blur");
 document.addEventListener("mousemove", function(dets){
-    crsr.style.left = dets.x-10+"px";
-    crsr.style.top = dets.y-10+"px";
+    crsr.style.left = dets.x+"px";
+    crsr.style.top = dets.y+"px";
     crsrBlur.style.left = dets.x - 250 +"px";
     crsrBlur.style.top = dets.y - 250 +"px";
 })
 
+// var h4all = document.querySelectorAll("#nav h4");
+// h4all.forEach(function(elem) {
+//     elem.addEventListener("mouseenter", function(){
+//         crsr.style.scale = 3
+//         crsr.style.border = "1px solid #fff"
+//         crsr.style.backgroundColor = "transparent"
+//     })
 
+//     elem.addEventListener("mouseleave", function(){
+//         crsr.style.scale = 1
+//         crsr.style.border = "0px solid #800080"
+//         crsr.style.backgroundColor = "#800080"
+//     })
+// })
 
 gsap.to('#nav', {
     backgroundColor : "#000",
@@ -32,5 +45,60 @@ gsap.to("#main", {
         start : "top -25%",
         end : "top -70%",
         scrub : 2
+    }
+})
+
+gsap.from("#About-us-in, #About-us img", {
+    y : 50,
+    opacity : 0,
+    duration : 1,
+    stagger : 0.4,
+    scrollTrigger : {
+        trigger : "#About-us",
+        scroller : "body",
+        // markers: true,
+        start : "top 60%",
+        end : "top 58%",
+        scrub: 3
+    }
+})
+
+// gsap.from(".card", {
+//     scale: 0.8,
+//     opacity : 0,
+//     duration : 1,
+//     scrollTrigger : {
+//         trigger : ".card",
+//         scroller : "body",
+//         // markers: true,
+//         start : "top 70%",
+//         end : "top 65%",
+//         scrub: 1
+//     }
+// })
+
+gsap.from("#katana1", {
+    y: -70,
+    duration : 1,
+    scrollTrigger : {
+        trigger : "#katana1",
+        scroller : "body",
+        // markers: true,
+        start : "top 55%",
+        end : "top 45%",
+        scrub: 3
+    }
+})
+
+gsap.from("#katana2", {
+    y: 70,
+    duration : 1,
+    scrollTrigger : {
+        trigger : "#katana1",
+        scroller : "body",
+        // markers: true,
+        start : "top 55%",
+        end : "top 45%",
+        scrub: 4
     }
 })
